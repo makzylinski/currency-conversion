668494e15d6c6309afec1b3fd0dafe84ea6130c6
Maks <makzylinski@gmail.com>
2024-06-16 20:40:16 +0200

    Introduced datepicker component

--- currency-conversion/src/app/app.module.ts
+++ currency-conversion/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { MatButtonModule } from '@angular/material/button';
 import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MatButtonModule, ReactiveFormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MatButtonModule, ReactiveFormsModule, DatepickerComponent]
 })
 export class AppModule {}
