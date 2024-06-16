ab0e1ab88c0b0b500196d3063a144711c340acf1
Maks <makzylinski@gmail.com>
2024-06-16 19:42:51 +0200

    setup and testing api

--- currency-conversion/src/app/app.component.ts
+++ currency-conversion/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'currency-conversion';
+export class AppComponent implements OnInit {
+  private readonly appService = inject(AppService);
+
+  ngOnInit(): void {
+    this.appService.getExchangeRates('A').subscribe(e => console.log(e))
+  }
 }
