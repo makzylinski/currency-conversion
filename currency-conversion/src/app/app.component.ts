2847d1ec722e71fdd54ea97c5e76205c53f0521c
Maks <makzylinski@gmail.com>
2024-06-16 20:48:43 +0200

    Added converter component

--- currency-conversion/src/app/app.component.ts
+++ currency-conversion/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AppService } from './services/app.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +8,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   private readonly appService = inject(AppService);
-  private readonly formBuilder = inject(FormBuilder);
-
-  currencyForm: FormGroup;
 
   ngOnInit(): void {
-    this.currencyForm = this.formBuilder.group({
-      amount: ['', [Validators.required, Validators.min(0)]],
-      currencyFrom: ['', Validators.required],
-      currencyTo: ['', Validators.required],
-    });
     this.appService.getExchangeRates('A').subscribe((e) => console.log(e));
   }
-
-  onConvertCurrency = () => null;
 }
---
+++ currency-conversion/src/app/components/converter/converter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-converter',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule],
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss'],
+})
+export class ConverterComponent implements OnInit {
+  private readonly formBuilder = inject(FormBuilder);
+  currencyForm: FormGroup;
+
+  ngOnInit(): void {
+    this.currencyForm = this.formBuilder.group({
+      amount: ['', [Validators.required, Validators.min(0)]],
+      currencyFrom: ['', Validators.required],
+      currencyTo: ['', Validators.required],
+    });
+  }
+
+  onConvertCurrency = () => null;
+}
