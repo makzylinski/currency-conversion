fb0219a6c7715e5b2e5192cbaa6bd98e7e999295
Maks <makzylinski@gmail.com>
2024-06-16 20:22:13 +0200

    added reactive forms and basic template and styles

--- currency-conversion/src/app/app.component.ts
+++ currency-conversion/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AppService } from './services/app.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,18 @@ import { AppService } from './services/app.service';
 })
 export class AppComponent implements OnInit {
   private readonly appService = inject(AppService);
+  private readonly formBuilder = inject(FormBuilder);
+
+  currencyForm: FormGroup;
 
   ngOnInit(): void {
-    this.appService.getExchangeRates('A').subscribe(e => console.log(e))
+    this.currencyForm = this.formBuilder.group({
+      amount: ['', [Validators.required, Validators.min(0)]],
+      currencyFrom: ['', Validators.required],
+      currencyTo: ['', Validators.required],
+    });
+    this.appService.getExchangeRates('A').subscribe((e) => console.log(e));
   }
+
+  onConvertCurrency = () => null;
 }
--- currency-conversion/src/app/app.module.ts
+++ currency-conversion/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MatButtonModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
