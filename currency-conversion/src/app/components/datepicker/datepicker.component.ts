2328e5ffab4cd8a1c95e2428fd21f436f45e5e21
Maks <makzylinski@gmail.com>
2024-06-17 11:42:03 +0200

    added current date to datepicker

--- currency-conversion/src/app/components/datepicker/datepicker.component.ts
+++ currency-conversion/src/app/components/datepicker/datepicker.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-datepicker',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './datepicker.component.html',
   styleUrls: ['./datepicker.component.scss']
 })
-export class DatepickerComponent {
+export class DatepickerComponent implements OnInit {
+  selectedDate: string;
+
+  ngOnInit(): void {
+    const today = new Date();
+    this.selectedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+  }
 
 }
