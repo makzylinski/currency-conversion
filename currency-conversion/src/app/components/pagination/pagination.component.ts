a9a42e547dba1ba08cb5f8e9697fec08ab5c53bc
Maks <makzylinski@gmail.com>
2024-06-18 12:30:03 +0200

    pagination mechanism ready

--- currency-conversion/src/app/components/pagination/pagination.component.ts
+++ currency-conversion/src/app/components/pagination/pagination.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.scss'
+  styleUrl: './pagination.component.scss',
 })
 export class PaginationComponent {
+  step = 0;
 
+  onStepBack = (): number => (this.step > 0 ? this.step-- : 0);
+
+  onStepForward = (): number => (this.step < 2 ? this.step++ : 0);
 }
