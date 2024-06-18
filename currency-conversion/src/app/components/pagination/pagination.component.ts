65e07a7b459201057392b1aa0e3de8101b6da032
Maks <makzylinski@gmail.com>
2024-06-18 12:45:32 +0200

    Added api request depending on pagination

--- currency-conversion/src/app/components/pagination/pagination.component.ts
+++ currency-conversion/src/app/components/pagination/pagination.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-pagination',
@@ -9,10 +10,41 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss',
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
+  private readonly appService = inject(AppService);
   step = 0;
 
-  onStepBack = (): number => (this.step > 0 ? this.step-- : 0);
+  ngOnInit(): void {
+    this.determineTable();
+  }
 
+  onStepBack = (): void => {
+    this.step > 0 ? this.step-- : 0;
+    this.determineTable();
+  };
   onStepForward = (): number => (this.step < 2 ? this.step++ : 0);
+
+  private determineTable = () => {
+    let table;
+    switch (this.step) {
+      case 0: {
+        table = 'A';
+        break;
+      }
+      case 1: {
+        table = 'B';
+        break;
+      }
+      case 2: {
+        table = 'C';
+        break;
+      }
+      default: {
+        table = 'A';
+        break;
+      }
+    }
+
+    this.appService.selectedTableSource$.next(table);
+  };
 }
--- currency-conversion/src/app/services/app.service.ts
+++ currency-conversion/src/app/services/app.service.ts
@@ -12,6 +12,7 @@ export class AppService {
   private readonly baseURL = 'http://api.nbp.pl/api/';
   private readonly http = inject(HttpClient);
   selectedDateSource$ = new BehaviorSubject<string>('');
+  selectedTableSource$ = new BehaviorSubject<string>('');
   currenciesDateSource$ = new BehaviorSubject<Rates[]>([]);
 
   getExchangeRates = (table: string, date: string): Observable<any> =>
@@ -19,7 +20,7 @@ export class AppService {
       .get<any>(`${this.baseURL}exchangerates/tables/${table}/${date}`)
       .pipe(tap((rates) => this.currenciesDateSource$.next(rates[0].rates)));
 
-      getCurrencies = (): Observable<Rates[]> => {
-        return this.currenciesDateSource$;
-      }
+  getCurrencies = (): Observable<Rates[]> => {
+    return this.currenciesDateSource$;
+  };
 }
